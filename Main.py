371bab4aecc46f26a86f7141e70a906c82314586
Zavaon Taylor <taylozav000@warren.k12.in.us>
2024-09-11 09:23:50 -0400

    Initial commit

---
+++ Main.py
@@ -0,0 +1,20 @@
+F_text = input("Enter the temp in degrees F:76 ")
+F = float(F_text)
+C = 5 * (F - 32) / 9
+print("The temp in degrees C is", C)
+
+C_text = input("Enter the temp in degrees C:26 ")
+
+F_text = input("Enter the temp in degrees F:89 ")
+F = float(F_text)
+C = 9 * (F - 32) / 8
+print("The temp in degrees c is", c)
+
+C_text = input("Enter the temp in degrees c:100 ")
+
+F_text = input("Enter the temp in degrees F:212 ")
+#INSTRUCTION: you do the rest.  Find the temperature in degrees F
+#This instruction should be 3 lines of code
+#Line 7 should be turning C_text into C with a casting function
+#line 8 should be calculating F using 1.8 * C + 32
+#line 9 should be printing the result like I did in line 4
